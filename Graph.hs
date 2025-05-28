aa689d6a82d379a114f53f8df4716cc027f239e0
IsacEkeroth <isac.ekeroth@icloud.com>
2025-05-28 13:19:30 +0200

    graph

--- Graph.hs
+++ Graph.hs
@@ -1,57 +1,67 @@
-module Graph 
+module Graph
   ( -- * Edge
-    Edge                    -- type
-  , src, dst, label         -- querying an Edge
+    Edge, -- type
+    src,
+    dst,
+    label, -- querying an Edge
 
     -- * Graph
-  , Graph                   -- type
-  , empty                   -- create an empty map
-  , addVertex, addVertices  -- adding vertices (nodes)
-  , addEdge, addBiEdge      -- adding edges (one way or both ways)
-  , adj                     -- get adjacent nodes for a given node
-  , vertices, edges         -- querying a Graph
-  ) where
+    Graph, -- type
+    empty, -- create an empty map
+    addVertex,
+    addVertices, -- adding vertices (nodes)
+    addEdge,
+    addBiEdge, -- adding edges (one way or both ways)
+    adj, -- get adjacent nodes for a given node
+    vertices,
+    edges, -- querying a Graph
+  )
+where
 
 import Data.Map (Map)
-import qualified Data.Map as M
+import Data.Map qualified as M
 import Data.Maybe
 
--- An edge with a source and destination node (of type a), 
+-- An edge with a source and destination node (of type a),
 -- together with a label of type b
 data Edge a b = Edge
-  { src   :: a  -- ^ Source vertex
-  , dst   :: a  -- ^ Destination vertex
-  , label :: b  -- ^ The label
-  } deriving Show
+  { -- | Source vertex
+    src :: a,
+    -- | Destination vertex
+    dst :: a,
+    -- | The label
+    label :: b
+  }
+  deriving (Show)
 
 -- A graph with nodes of type a and labels of type b.
-data Graph a b = Graph -- TODO: implement a graph with adjacency lists, hint: use a Map.
+data Graph a b = Graph (Map a [Edge a b]) deriving (Show) -- TODO: implement a graph with adjacency lists, hint: use a Map.
 
 -- | Create an empty graph
 empty :: Graph a b
-empty = undefined
+empty = Graph M.empty
 
 -- | Add a vertex (node) to a graph
-addVertex :: Ord a => a -> Graph a b -> Graph a b
+addVertex :: (Ord a) => a -> Graph a b -> Graph a b
 addVertex v g = undefined
 
 -- | Add a list of vertices to a graph
-addVertices :: Ord a => [a] -> Graph a b -> Graph a b
+addVertices :: (Ord a) => [a] -> Graph a b -> Graph a b
 addVertices vs g = undefined
 
--- | Add an edge to a graph, the first parameter is the start vertex (of type a), 
+-- | Add an edge to a graph, the first parameter is the start vertex (of type a),
 -- the second parameter the destination vertex, and the third parameter is the
 -- label (of type b)
-addEdge :: Ord a => a -> a -> b -> Graph a b -> Graph a b
+addEdge :: (Ord a) => a -> a -> b -> Graph a b -> Graph a b
 addEdge v w l = undefined
 
 -- | Add an edge from start to destination, but also from destination to start,
 -- with the same label.
-addBiEdge :: Ord a => a -> a -> b -> Graph a b -> Graph a b
+addBiEdge :: (Ord a) => a -> a -> b -> Graph a b -> Graph a b
 addBiEdge v w l = undefined
 
 -- | Get all adjacent vertices (nodes) for a given node
-adj :: Ord a => a -> Graph a b -> [Edge a b]
+adj :: (Ord a) => a -> Graph a b -> [Edge a b]
 adj v g = undefined
 
 -- | Get all vertices (nodes) in a graph
---
+++ Main.hs
@@ -0,0 +1,14 @@
+import Graph
+import Graphics.UI.Threepenny (content)
+import Route
+import RouteGUI
+import Prelude
+import Prelude (putStrLn)
+
+main :: IO ()
+main = do
+  --   contents <- getContents
+
+  --     let wordsList = words contents
+  --     putStrLn $ show wordsList
+  putStrLn "test"
